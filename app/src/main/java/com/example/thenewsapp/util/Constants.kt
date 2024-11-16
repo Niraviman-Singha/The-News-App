e91f3599581491f2198be530c8201f24d5970e61
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-11-16 12:04:34 +0600

    Create an interface NewsAPI where get the news top headlines and news article also.

---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "359d43f001c04156acd37dd897f39ee0"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = "500L"
+        const val QUERY_PAGE_SIZE = "20"
+    }
+}
