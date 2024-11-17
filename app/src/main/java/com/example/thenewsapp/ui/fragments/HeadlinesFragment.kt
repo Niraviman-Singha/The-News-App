ba54488fc1b8753b1e6f5136708a7752b47986a6
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-11-17 19:03:34 +0600

    ensure to implement error handling and consider showing a loading indicator for better UX!

--- app/src/main/java/com/example/thenewsapp/ui/fragments/HeadlinesFragment.kt
+++ app/src/main/java/com/example/thenewsapp/ui/fragments/HeadlinesFragment.kt
@@ -8,18 +8,21 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.thenewsapp.R
 import com.example.thenewsapp.adapters.NewsAdapter
 import com.example.thenewsapp.databinding.FragmentHeadlinesBinding
 import com.example.thenewsapp.ui.NewsViewModel
+import com.example.thenewsapp.util.Constants
 
 
 class HeadlinesFragment : Fragment(R.layout.fragment_headlines) {
     lateinit var newsViewModel: NewsViewModel
     lateinit var newsAdapter: NewsAdapter
     lateinit var retryButton: Button
-    lateinit var errorText:TextView
-    lateinit var itemHeadlinesError:CardView
+    lateinit var errorText: TextView
+    lateinit var itemHeadlinesError: CardView
 
     lateinit var binding: FragmentHeadlinesBinding
 
@@ -35,25 +38,54 @@ class HeadlinesFragment : Fragment(R.layout.fragment_headlines) {
     var isLastPage = false
     var isScrolling = false
 
-    private fun hideProgressbar(){
+    private fun hideProgressbar() {
         binding.paginationProgressBar.visibility = View.INVISIBLE
         isLoading = false
     }
 
-    private fun showProgressbar(){
+    private fun showProgressbar() {
         binding.paginationProgressBar.visibility = View.VISIBLE
         isLoading = true
     }
 
-    private fun hideErrorMessage(){
+    private fun hideErrorMessage() {
         itemHeadlinesError.visibility = View.INVISIBLE
         isError = false
     }
 
-    private fun showErrorMessage(message:String){
+    private fun showErrorMessage(message: String) {
         itemHeadlinesError.visibility = View.VISIBLE
         errorText.text = message
         isError = true
+    }
+
+    val scrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isNoErrors = !isError
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible = totalItemCount >= Constants.QUERY_PAGE_SIZE
+            val shouldPaginate = isNoErrors && isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning && isTotalMoreThanVisible && isScrolling
+
+            if (shouldPaginate){
+                newsViewModel.getHeadlines("us")
+                isScrolling = false
+            }
+
+
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+        }
     }
 
 
