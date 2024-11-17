cc27cda8e7eb9c545a9af1a5d5d84e0812b1bf56
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-11-17 12:02:03 +0600

    Initializes the NewsActivity and sets up a NewsViewModel with a NewsRepository to manage news articles.

--- app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/thenewsapp/ui/NewsActivity.kt
@@ -2,11 +2,24 @@ package com.example.thenewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.thenewsapp.R
+import com.example.thenewsapp.database.ArticleDatabase
+import com.example.thenewsapp.databinding.ActivityNewsBinding
+import com.example.thenewsapp.repository.NewsRepository
+import com.example.thenewsapp.util.NewsViewModelProviderFactory
 
 class NewsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityNewsBinding
+    lateinit var newsViewModel: NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+        newsViewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
     }
 }
--- app/src/main/java/com/example/thenewsapp/util/NewsViewModelProviderFactory.kt
+++ app/src/main/java/com/example/thenewsapp/util/NewsViewModelProviderFactory.kt
@@ -7,6 +7,7 @@ import com.example.thenewsapp.repository.NewsRepository
 import com.example.thenewsapp.ui.NewsViewModel
 
 class NewsViewModelProviderFactory(val app:Application, val newsRepository: NewsRepository):ViewModelProvider.Factory {
+
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return NewsViewModel(app, newsRepository) as T
     }
