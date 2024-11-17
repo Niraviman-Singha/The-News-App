9ac4a90ffacbcbe5e8ca2c719d35b7c9c556ca2a
Niraviman-Singha <niraviman15-11689@diu.edu.bd>
2024-11-17 22:06:23 +0600

    sets up the RecyclerView for displaying news articles, observes the ViewModel for data updates.

--- app/src/main/java/com/example/thenewsapp/ui/fragments/HeadlinesFragment.kt
+++ app/src/main/java/com/example/thenewsapp/ui/fragments/HeadlinesFragment.kt
@@ -1,20 +1,27 @@
 package com.example.thenewsapp.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.thenewsapp.R
 import com.example.thenewsapp.adapters.NewsAdapter
 import com.example.thenewsapp.databinding.FragmentHeadlinesBinding
+import com.example.thenewsapp.ui.NewsActivity
 import com.example.thenewsapp.ui.NewsViewModel
 import com.example.thenewsapp.util.Constants
+import com.example.thenewsapp.util.Resource
 
 
 class HeadlinesFragment : Fragment(R.layout.fragment_headlines) {
@@ -28,8 +35,60 @@ class HeadlinesFragment : Fragment(R.layout.fragment_headlines) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         binding = FragmentHeadlinesBinding.bind(view)
+
+        itemHeadlinesError = view.findViewById(R.id.itemHeadlinesError)
+
+        val inflater = requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view:View = inflater.inflate(R.layout.item_error, null)
+
+        retryButton = view.findViewById(R.id.retryButton)
+        errorText = view.findViewById(R.id.errorText)
+
+        newsViewModel = (activity as NewsActivity).newsViewModel
+        setupHeadlinesRecycler()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(R.id.action_headlinesFragment_to_articleFragment, bundle)
+        }
+
+        newsViewModel.headlines.observe(viewLifecycleOwner, Observer { response->
+            when(response){
+                is Resource.Success<*> ->{
+                    hideProgressbar()
+                    hideErrorMessage()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles.toList())
+                        val totalPages = newsResponse.totalResults / Constants.QUERY_PAGE_SIZE + 2
+                        isLastPage = newsViewModel.headlinesPage == totalPages
+                        if (isLastPage){
+                            binding.recyclerHeadlines.setPadding(0,0,0,0)
+                        }
+                    }
+                }
+                is Resource.Error<*> ->{
+                    hideProgressbar()
+                    response.message?.let { message->
+                        Toast.makeText(activity, "Sorry error: $message", Toast.LENGTH_LONG).show()
+                        showErrorMessage(message)
+                    }
+
+                }
+                is Resource.Loading<*> ->{
+                    showProgressbar()
+
+                }
+            }
+        })
+
+        retryButton.setOnClickListener {
+            newsViewModel.getHeadlines("us")
+        }
+
+
 
     }
 
@@ -73,9 +132,10 @@ class HeadlinesFragment : Fragment(R.layout.fragment_headlines) {
             val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
             val isNotAtBeginning = firstVisibleItemPosition >= 0
             val isTotalMoreThanVisible = totalItemCount >= Constants.QUERY_PAGE_SIZE
-            val shouldPaginate = isNoErrors && isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning && isTotalMoreThanVisible && isScrolling
+            val shouldPaginate =
+                isNoErrors && isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning && isTotalMoreThanVisible && isScrolling
 
-            if (shouldPaginate){
+            if (shouldPaginate) {
                 newsViewModel.getHeadlines("us")
                 isScrolling = false
             }
@@ -85,8 +145,22 @@ class HeadlinesFragment : Fragment(R.layout.fragment_headlines) {
 
         override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
             super.onScrollStateChanged(recyclerView, newState)
+
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true
+            }
         }
+
     }
 
+        private fun setupHeadlinesRecycler() {
+            newsAdapter = NewsAdapter()
+            binding.recyclerHeadlines.apply {
+                adapter = newsAdapter
+                layoutManager = LinearLayoutManager(activity)
+                addOnScrollListener(this@HeadlinesFragment.scrollListener)
+            }
+
+        }
 
 }
